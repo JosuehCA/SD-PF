9b233f091b2cf01830e27db327a66b928801f5c0
JosuehCA <josuemago2012@gmail.com>
2026-05-23 16:29:32 -0600

    Definición inicial de modelos y lógica de negocio (views)

--- sistema/consultorio/models.py
+++ sistema/consultorio/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Paciente(AbstractUser):
+    """
+    Entidad paciente
+
+    Características:
+        Nombre
+        Dirección
+        Correo Electrónico
+        Teléfono
+        Edad
+        Sexo
+
+    Identificación mediante nombre de usuario y contraseña
+
+    Se debe permitir la consulta de sus datos por parte del paciente mismo y del médico, y la modificación y eliminación por parte del
+    médico
+    """
+
+    pass
+
+class Cita(models.Model):
+    """
+    Entidad cita
+
+    Características:
+        Día
+        Hora
+    
+    Se debe permitir su consulta, modificación y eliminación, tanto por parte del paciente como del médico
+
+    Se manejan notificaciones para el paciente en caso de tener
+    una nueva cita o haber sido eliminada
+    """
+
+    pass
+
+class Consulta(models.Model):
+    """
+    Entidad consulta. Representa una consulta realizada sobre un paciente
+
+    Características:
+        Temperatura Corporal
+        Peso
+        Altura
+        Presión arterial
+
+    INFORMACIÓN ENCRIPTADA
+    """
+
+    pass
+
+class Historial(models.Model):
+    """
+    Entidad historial, generada por el médico sobre lo que ocurre en la consulta con el paciente
+
+    Características:
+        ???
+    """
+
+    pass
--- sistema/consultorio/views.py
+++ sistema/consultorio/views.py
@@ -1,4 +1,29 @@
 from django.http import HttpResponse
+from . import models
 
 def index(request):
     return HttpResponse("Hello, world. You're at the consultorio index.")
+
+def generarReporte(request):
+    pass
+
+def generarHistorial(request):
+    pass
+
+def crearPaciente(request):
+    pass
+
+def consultarDatosDePaciente(request):
+    pass
+
+def crearCita(request):
+    pass
+
+def consultarDatosDeCita(request):
+    pass
+
+def crearConsulta(request):
+    pass
+
+def obtenerDatosDeConsulta(request):
+    pass
